5a22ecb0d08571b2313e85ecfbe333cd2c22056e
Bryan Schauerte <bryan.schauerte@gmail.com>
2015-06-13 03:43:58 +0900

    initial heroku push

--- pinterested2/config/routes.rb
+++ pinterested2/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  #roots have a #  and are called root rather than get
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
